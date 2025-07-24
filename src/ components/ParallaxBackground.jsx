5ccb66dcf4ed487dd4f9984d23767f2689994c22
Pradeesh1108 <pradeeshsivaprakasam@gmail.com>
2025-07-25 00:49:19 +0530

    completed the navbar and the hero section

---
+++ src/ components/ParallaxBackground.jsx
@@ -0,0 +1,12 @@
+const ParallaxBackground = () => {
+  return (
+    <section className="absolute inset-0 bg-black/40">
+        <div className="relative h-screen overflow-y-hidden">
+            <div />
+
+        </div>
+    </section>
+  )
+}
+
+export default ParallaxBackground
